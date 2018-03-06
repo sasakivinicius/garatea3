3be267523c88a837de3538afb4e5829c0b270770
YagoMello <32549702+YagoMello@users.noreply.github.com>
2018-03-06 13:12:39 -0300

    Inserindo o código de compressão mais atualizado (zipstring / unzipstring)
    E alinhando uns comments. Deletei a main de teste e tirei uns defines que ja estão incluidos no master.

--- master/EEPROM.cpp
+++ master/EEPROM.cpp
@@ -3,12 +3,12 @@
  *
  * Created: 20/10/2017 16:23:50
  * Author : Boludo
- */ 
- #define F_CPU 8000000
+ */
+//descomenta se der ruim, deve ter a ver com o CLOCKS_PER_SEC
+//#define F_CPU 8000000
 
 #include <avr/io.h>
 #include <inttypes.h>
-#include "C:\Program Files (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src\Wire.h"
 #include <time.h>
 #include <util/delay.h>
 
@@ -16,35 +16,35 @@ class eeprom{
 public:
 	/* error id
 	d7 - desligamento inesperado do sistema 
-	d6 - letra nao premitida durante compressão
+	d6 - letra nao premitida durante compressÃ£o
 	d5 - 
 	d4 - 
 	d3 - 
-	d2 - tentativa de ler em uma posição inacessivel (sem permissao ou maior que a memoria)
-	d1 - tentativa de escrever em uma posição inacessivel (sem permissao ou maior que a memoria)
-	d0 - tentativa de acessar uma posição inacessivel (sem permissao ou maior que a memoria)
+	d2 - tentativa de ler em uma posiÃ§Ã£o inacessivel (sem permissao ou maior que a memoria)
+	d1 - tentativa de escrever em uma posiÃ§Ã£o inacessivel (sem permissao ou maior que a memoria)
+	d0 - tentativa de acessar uma posiÃ§Ã£o inacessivel (sem permissao ou maior que a memoria)
 	*/
 	/* bytes reservados
 	0x00 mempos[1]
 	0x01 mempos[0]
 	0x02 err
 	*/
-	//ziptable										//tabela de compactação (pode colocar char)
-	#define to0x0A 0xFF
-	#define to0x0B 0xFF
-	#define to0x0C 0xFF
-	#define to0x0D 0xFF
+	//ziptable					//tabela de compactaÃ§Ã£o (pode colocar char)
+	#define to0x0A 'n'
+	#define to0x0B 'e'
+	#define to0x0C 's'
+	#define to0x0D 'w'
 	#define to0x0E 0xFF
 	#define to0x0F 0x0A // \n
 	//agr vem o resto
-	#define operationdelay 5						//tempo minimo de espera entre operações
-	#define reserved 3								//numero de bytes reservados no começo da memoria
-	#define pagesize 64 							//tamanho maximo de uma page
-	#define memorysize 32768						//tamanho da memoria em bytes
-	uint16_t memcount;								//variavel auxiliar para esctira
-	uint8_t err;									//log de erros
+	#define operationdelay 5			//tempo minimo de espera entre operaÃ§Ãµes
+	#define reserved 3				//numero de bytes reservados no comeÃ§o da memoria
+	#define pagesize 64 				//tamanho maximo de uma page
+	#define memorysize 32768			//tamanho da memoria em bytes
+	uint16_t memcount;				//variavel auxiliar para esctira
+	uint8_t err;					//log de erros
 	clock_t lastcall;
-	void delayquepresta(uint16_t dms){				//em decimo de ms. _delay_ms requer variavel definida no tempo de compilação
+	void delayquepresta(uint16_t dms){		//em decimo de ms. _delay_ms requer variavel definida no tempo de compilaÃ§Ã£o
 		while(dms!=0){
 			_delay_us(100);
 			dms--;
@@ -55,13 +55,13 @@ public:
 			delayquepresta(delay - ((10000*(clock()-lasttime))/CLOCKS_PER_SEC));
 		}
 	}
-	typedef union{									//union para auxiliar na quebra do endereço principal da memoria
+	typedef union{						//union para auxiliar na quebra do endereÃ§o principal da memoria
 		uint16_t full;
 		uint8_t half[2];
 	}memdiv;
-	memdiv mempos;									//aponta para a posição principal na eeprom
-	memdiv temppos;									//aponta para uma posição temporaria na eeprom
-	void gotobyte(uint16_t x){						//move o ponto em que estão sendo salvos os dados na eeprom
+	memdiv mempos;						//aponta para a posiÃ§Ã£o principal na eeprom
+	memdiv temppos;						//aponta para uma posiÃ§Ã£o temporaria na eeprom
+	void gotobyte(uint16_t x){				//move o ponto em que estÃ£o sendo salvos os dados na eeprom
 		if((x < memorysize) && (x > reserved)){
 			mempos.full= x;
 		}
@@ -69,7 +69,7 @@ public:
 			err=err | 0x01;
 		}
 	}
-	void writebyte(uint16_t pos, uint8_t data){		//escreve um unico byte na posiçao especificada, sem restrições
+	void writebyte(uint8_t data, uint16_t pos){		//escreve um unico byte na posiÃ§ao especificada, sem restriÃ§Ãµes
 		if(pos < memorysize){
 			mindelay(operationdelay, lastcall);
 			temppos.full=pos;
@@ -84,7 +84,7 @@ public:
 			err=err | 0x02;
 		}
 	}
-	uint8_t readbyte(uint16_t pos){					//le um unico byte na posição especificada, sem restrições
+	uint8_t readbyte(uint16_t pos){				//le um unico byte na posiÃ§Ã£o especificada, sem restriÃ§Ãµes
 		if(pos < memorysize){
 			Wire.beginTransmission(0b10100000);
 			Wire.write(temppos.half[1]);
@@ -98,7 +98,7 @@ public:
 		}
 		return 0;
 	}
-	void init(void){								//inicializa a memoria (favor usar apenas uma vez), e checa se houve um desligamento inesperado
+	void init(void){					//inicializa a memoria (favor usar apenas uma vez), e checa se houve um desligamento inesperado
 		mempos.half[1]=readbyte(0x00);
 		mempos.half[0]=readbyte(0x01);
 		err=readbyte(0x02);
@@ -111,7 +111,7 @@ public:
 			err=err | 0x80;
 		}
 	}
-	void writestring(char *data){					//escreve uma string na eeprom
+	void writestring(char *data){				//escreve uma string na eeprom
 		uint16_t offset=0;
 		uint8_t temcoisa=1;
 		while(temcoisa){
@@ -133,7 +133,7 @@ public:
 			mempos.full+=memcount;
 		}
 	}
-	void readpage(char *buffer,uint16_t pos){					//escreve uma string na eeprom
+	void readpage(uint16_t pos, char *buffer){		//escreve uma string na eeprom
 		uint16_t offset=0;
 		uint8_t temcoisa=0;
 		memcount = 0;
@@ -155,72 +155,113 @@ public:
 			mempos.full+= memcount;
 		}
 	}
-	void updatelogs(void){										//atualiza os dados de erro e posição de memoria na eeprom
+	void updatelogs(void){					//atualiza os dados de erro e posiÃ§Ã£o de memoria na eeprom
 		writebyte(0x00,mempos.half[1]);
 		writebyte(0x01,mempos.half[0]);
 		writebyte(0x02,err);
 	}
-	void resetlogs(){											//reseta os dados de erro e posição salva na memoria da eeprom
+	void resetlogs(void){					//reseta os dados de erro e posiÃ§Ã£o salva na memoria da eeprom
 		writebyte(0x00,0x00);
 		writebyte(0x01,0x00);
 		writebyte(0x02,0x00);
 	}
-	uint16_t zipstring(char *inp, char *out){
-		uint16_t pos=0;
-		uint16_t outpos=0;
+	uint16_t zipstring(char *inp, char *out) {
+		uint16_t pos = 0;
+		uint16_t outpos = 0;
 		uint8_t esquerda;
-		uint8_t temcoisa=1;
-		while(temcoisa){
-			if(pos & 0x01){
+		uint8_t temcoisa = 1;
+		while (temcoisa) {
+			if (!(pos & 0x01)) {
 				esquerda = 4;
-				out[outpos]=0;
+				out[outpos] = 0;
 			}
-			else{
-				esquerda=0;
+			else {
+				esquerda = 0;
 			}
-			if((0x30<=inp[pos])&&(inp[pos]<=0x39)){
-				out[outpos]+=(inp[pos] - 0x29)<<esquerda;
+			if ((0x30 <= inp[pos]) && (inp[pos] <= 0x39)) {
+				out[outpos] += (inp[pos] - 0x2F) << esquerda;
 			}
-			else if(inp[pos]==to0x0B){
-				out[outpos]+=(0x0B)<<esquerda;
+			else if (inp[pos] == to0x0B) {
+				out[outpos] += 0x0B << esquerda;
 			}
-			else if(inp[pos]==to0x0C){
-				out[outpos]+=(0x0C)<<esquerda;
+			else if (inp[pos] == to0x0C) {
+				out[outpos] += 0x0C << esquerda;
 			}
-			else if(inp[pos]==to0x0D){
-				out[outpos]+=(0x0D)<<esquerda;
+			else if (inp[pos] == to0x0D) {
+				out[outpos] += 0x0D << esquerda;
 			}
-			else if(inp[pos]==to0x0E){
-				out[outpos]+=(0x0E)<<esquerda;
+			else if (inp[pos] == to0x0E) {
+				out[outpos] += 0x0E << esquerda;
 			}
-			else if(inp[pos]==to0x0F){
-				out[outpos]+=(0x0F)<<esquerda;
+			else if (inp[pos] == to0x0F) {
+				out[outpos] += 0x0F << esquerda;
 			}
-			else if(inp[pos]=='\0'){
-				if(esquerda){
-					out[outpos]='\0';
+			else if (inp[pos] == '\0') {
+				if (esquerda) {
+					out[outpos] = '\0';
 				}
+				temcoisa = 0;
 			}
-			else{
-				err=0x40;
+			else {
+				err = 0x40;
 			}
+			//printf("%c %x %d\n", out[outpos], out[outpos], esquerda);
 			pos++;
-			if(!esquerda){
+			if (!esquerda) {
 				outpos++;
 			}
 		}
 		return outpos;
 	}
-	void unzipstring(char *inp, char *out){
-		
+	uint16_t unzipstring(char *inp, char *out) {
+		uint16_t pos = 0;
+		uint16_t outpos = 0;
+		uint8_t temcoisa = 1;
+		while ((inp[pos] != '\0') &&  (temcoisa)) {
+			out[outpos] = (inp[pos] & 0xF0) >> 4;
+			//printf("%c %x %d\n", out[outpos], out[outpos], 4);
+			outpos++;
+			if ((inp[pos] & 0x0F) != '\0') {
+				out[outpos] = inp[pos] & 0x0F;
+				//printf("%c %x %d\n", out[outpos], out[outpos], 0);
+				outpos++;
+			}
+			else {
+				temcoisa = 0;
+			}
+			pos++;
+		}
+		out[outpos] = '\0';
+		outpos = 0;
+		temcoisa = 1;
+		while (temcoisa) {
+			if ((0x01 <= out[outpos]) && (out[outpos] <= 0x0A)) {
+				out[outpos] = (out[outpos] + 0x2F);
+			}
+			else if (out[outpos] == 0x0B) {
+				out[outpos] = to0x0B;
+			}
+			else if (out[outpos] == 0x0C) {
+				out[outpos] = to0x0C;
+			}
+			else if (out[outpos] == 0x0D) {
+				out[outpos] = to0x0D;
+			}
+			else if (out[outpos] == 0x0E) {
+				out[outpos] = to0x0E;
+			}
+			else if (out[outpos] == 0x0F) {
+				out[outpos] = to0x0F;
+			}
+			else if (out[outpos] == '\0') {
+				temcoisa = 0;
+			}
+			else {
+				err = 0x40;
+			}
+			outpos++;
+		}
+		out[outpos] = '\0';
+		return outpos;
 	}
 }eeprom;
-
-
-int main(void){
-	eeprom.init();
-    while(1){	
-	}
-	return 0;
-}
-
